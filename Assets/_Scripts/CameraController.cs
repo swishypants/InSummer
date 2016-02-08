971098f2defb6464b8069189dbeb2cd2afeaebab
swishypants <shamuflute@gmail.com>
2016-02-07 20:21:32 -0500

    rawr

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public float speed;
+	public float rotateSpeed;
+
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	void Update() {
+		// mouse
+		transform.Rotate(0, Input.GetAxis ("Mouse X") * rotateSpeed, 0);
+
+		// movements
+		/*if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += (-1 * transform.right) * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+			transform.position += transform.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) {
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)) {
+			transform.position += (-1 * transform.forward) * speed * Time.deltaTime;
+		}*/
+
+		// modify height
+		if (Input.GetKey (KeyCode.Q)) {
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.E)) {
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+	}
+
+	void LateUpdate () {
+		//transform.position = new Vector3 (player.transform.position.x, 0, player.transform.position.z);
+		transform.position = player.transform.position;
+	}
+}
---
+++ Assets/_Scripts/PlayerScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed;
+	private int score;
+	public Text scoreText;
+	public Text winText;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		setScore (score);
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += (-1 * Camera.main.transform.right) * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+			transform.position += Camera.main.transform.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) {
+			transform.position += Camera.main.transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)) {
+			transform.position += (-1 * Camera.main.transform.forward) * speed * Time.deltaTime;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Snowflake")) {
+			other.gameObject.SetActive(false);
+			setScore (++score);
+			if (score == 6) {
+				winText.text = "You won by staying cool with snowflakes and defeating summer!";
+			}
+		}
+	}
+
+	void setScore(int count) {
+		scoreText.text = "Score: " + count.ToString ();
+	}
+}
