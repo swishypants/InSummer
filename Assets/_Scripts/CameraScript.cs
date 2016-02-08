1f52ca60fc4f664b67f1bed824e6a0884cea4dd3
swishypants <shamuflute@gmail.com>
2016-02-07 22:01:23 -0500

    derp

---
+++ Assets/_Scripts/CameraScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Camera fps_camera;
+	public Camera god_camera;
+
+	public GameObject player;
+	public float speed;
+	public float rotateSpeed;
+
+	void Start () {
+		fps_camera.enabled = true;
+		god_camera.enabled = false;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	void Update() {
+		// modify height
+		if (fps_camera.isActiveAndEnabled) {
+			if (Input.GetKey (KeyCode.Q)) {
+				Camera.main.transform.position += Vector3.up * speed * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.E)) {
+				Camera.main.transform.position += Vector3.down * speed * Time.deltaTime;
+			}
+		}
+
+		// switch cameras
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			fps_camera.enabled = !fps_camera.enabled;
+			god_camera.enabled = !god_camera.enabled;
+		}
+	}
+
+	void LateUpdate() {
+		if (fps_camera.isActiveAndEnabled) {
+			Camera.main.transform.Rotate (0, Input.GetAxis ("Mouse X") * rotateSpeed, 0);
+		}
+	}
+}
--- Assets/_Scripts/PlayerScript.cs
+++ Assets/_Scripts/PlayerScript.cs
@@ -18,17 +18,43 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-			transform.position += (-1 * Camera.main.transform.right) * speed * Time.deltaTime;
+		// fps camera movement
+		if (Camera.main.transform.forward.y == 1) {
+			if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+				transform.position += (-1 * Camera.main.transform.right) * speed * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
+				transform.position += Camera.main.transform.right * speed * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) {
+				transform.position += Camera.main.transform.forward * speed * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)) {
+				transform.position += (-1 * Camera.main.transform.forward) * speed * Time.deltaTime;
+			}
 		}
-		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
-			transform.position += Camera.main.transform.right * speed * Time.deltaTime;
+		// god's eye camera movement
+		else {
+			if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+				transform.position += (-1 * Camera.main.transform.right) * speed * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
+				transform.position += Camera.main.transform.right * speed * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) {
+				transform.position += (Vector3.Cross(transform.right.normalized, transform.up.normalized)) * speed * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)) {
+				transform.position += (-1 * (Vector3.Cross(transform.right.normalized, transform.up.normalized))) * speed * Time.deltaTime;
+			}
 		}
-		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) {
-			transform.position += Camera.main.transform.forward * speed * Time.deltaTime;
+
+		// change height
+		if (Input.GetKey (KeyCode.Q)) {
+			Camera.main.transform.position += Vector3.up * speed * Time.deltaTime;
 		}
-		if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)) {
-			transform.position += (-1 * Camera.main.transform.forward) * speed * Time.deltaTime;
+		if (Input.GetKey (KeyCode.E)) {
+			Camera.main.transform.position += Vector3.down * speed * Time.deltaTime;
 		}
 	}
 
